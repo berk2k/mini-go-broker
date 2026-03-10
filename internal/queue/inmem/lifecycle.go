9fef61edb528bf42746fad2422b7e472d6b580e9
Berk Polat <berkpolat125@gmail.com>
2026-03-10 17:41:16 +0300

    feat: helpers

--- internal/queue/inmem/lifecycle.go
+++ internal/queue/inmem/lifecycle.go
@@ -2,11 +2,40 @@ package inmem
 
 import "time"
 
+func (q *Queue) RequeueAllForConsumer(consumerID string) {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	for id, lease := range q.inflight {
+		if lease.ConsumerID == consumerID {
+
+			lease.Message.Attempts++
+
+			q.inflightCount[consumerID]--
+
+			if lease.Message.Attempts >= q.maxRetries {
+				q.addToDLQ(lease.Message)
+			} else {
+
+				q.ready = append(q.ready, DelayedMessage{
+					Message: lease.Message,
+					ReadyAt: time.Now(),
+				})
+
+				q.cond.Signal()
+			}
+
+			delete(q.inflight, id)
+		}
+	}
+}
+
 func (q *Queue) ForceRequeueAll() {
 	q.mu.Lock()
 	defer q.mu.Unlock()
 
 	for id, lease := range q.inflight {
+
 		lease.Message.Attempts++
 
 		if lease.Message.Attempts >= q.maxRetries {
@@ -19,32 +48,6 @@ func (q *Queue) ForceRequeueAll() {
 		}
 
 		delete(q.inflight, id)
-	}
-
-	q.cond.Broadcast()
-}
-
-func (q *Queue) RequeueAllForConsumer(consumerID string) {
-	q.mu.Lock()
-	defer q.mu.Unlock()
-
-	for id, lease := range q.inflight {
-		if lease.ConsumerID == consumerID {
-
-			lease.Message.Attempts++
-
-			if lease.Message.Attempts >= q.maxRetries {
-				q.addToDLQ(lease.Message)
-			} else {
-				q.ready = append(q.ready, DelayedMessage{
-					Message: lease.Message,
-					ReadyAt: time.Now(),
-				})
-			}
-
-			q.inflightCount[consumerID]--
-			delete(q.inflight, id)
-		}
 	}
 
 	q.cond.Broadcast()
